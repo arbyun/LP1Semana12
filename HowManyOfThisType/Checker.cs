305a5a65030eb34c696e02bb4c4e6d41f532d9a0
arbyun <someonealiveh@gmail.com>
2023-06-02 10:49:47 +0100

    create HowManyOfTipe method

--- HowManyOfThisType/Checker.cs
+++ HowManyOfThisType/Checker.cs
@@ -1,7 +1,20 @@
-﻿namespace HowManyOfThisType
+﻿using System.Collections.Generic;
+
+namespace HowManyOfThisType
 {
     public class Checker
     {
-        
+        public static int HowManyOfType<T>(IEnumerable<object> items)
+        {
+            int count = 0;
+            foreach (var item in items)
+            {
+                if (item is T)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
