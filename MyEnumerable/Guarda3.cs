8a638a905ccd8b492d50c44a905dfdecfc663e07
arbyun <someonealiveh@gmail.com>
2023-06-02 11:04:24 +0100

    copied class Guard3.cs to MyEnumerable project

---
+++ MyEnumerable/Guarda3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyEnumerable
+{
+    public class Guarda3<T>
+    {
+        private T item1;
+        private T item2;
+        private T item3;
+        
+        public Guarda3()
+        {
+            item1 = default(T);
+            item2 = default(T);
+            item3 = default(T);
+        }
+        
+        public T GetItem(int i)
+        {
+            if (i == 0)
+                return item1;
+            else if (i == 1)
+                return item2;
+            else if (i == 2)
+                return item3;
+            else
+                throw new IndexOutOfRangeException();
+        }
+        
+        public void SetItem(int i, T item)
+        {
+            if (i == 0)
+                item1 = item;
+            else if (i == 1)
+                item2 = item;
+            else if (i == 2)
+                item3 = item;
+            else
+                throw new IndexOutOfRangeException();
+        }
+    }
+}
