4a5b7d52432de09010671a25fcf823fae199bb1c
arbyun <someonealiveh@gmail.com>
2023-06-02 10:54:34 +0100

    implement static method GetTheOnesBefore()

--- TheyCameBefore/BeforeUtils.cs
+++ TheyCameBefore/BeforeUtils.cs
@@ -1,7 +1,24 @@
-﻿namespace TheyCameBefore
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheyCameBefore
 {
-    public class BeforeUtils
+    public static class BeforeUtils
     {
-        
+        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T item)
+            where T : struct, IComparable<T>
+        {
+            List<T> result = new List<T>();
+
+            foreach (var currentItem in items)
+            {
+                if (currentItem.CompareTo(item) < 0)
+                {
+                    result.Add(currentItem);
+                }
+            }
+
+            return result;
+        }
     }
 }
