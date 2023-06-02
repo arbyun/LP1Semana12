154198872bc36322d5962fbd8336b79328c7021c
arbyun <someonealiveh@gmail.com>
2023-06-02 11:06:21 +0100

    create foreach loop for iterating through Guard3 instances

--- MyEnumerable/Program.cs
+++ MyEnumerable/Program.cs
@@ -6,7 +6,29 @@ namespace MyEnumerable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Guarda3<string> guarda3String = new Guarda3<string>();
+            guarda3String.SetItem(0, "Item 1");
+            guarda3String.SetItem(1, "Item 2");
+            guarda3String.SetItem(2, "Item 3");
+
+            Console.WriteLine("Guarda3<string> items:");
+            foreach (var item in guarda3String)
+            {
+                Console.WriteLine(item);
+            }
+
+            Guarda3<float> guarda3Float = new Guarda3<float>();
+            guarda3Float.SetItem(0, 1.23f);
+            guarda3Float.SetItem(1, 4.56f);
+            guarda3Float.SetItem(2, 7.89f);
+
+            Console.WriteLine("Guarda3<float> items:");
+            foreach (var item in guarda3Float)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
