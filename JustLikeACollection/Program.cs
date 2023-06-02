a45ac27bc8f3943df418b40def6144acae9bcb72
arbyun <someonealiveh@gmail.com>
2023-06-02 11:11:13 +0100

    once again instantiate Guarda3 in Program as a collection

--- JustLikeACollection/Program.cs
+++ JustLikeACollection/Program.cs
@@ -6,7 +6,33 @@ namespace JustLikeACollection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Guarda3<string> guarda3String = new Guarda3<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3"
+            };
+
+            Console.WriteLine("Guarda3<string> items:");
+            foreach (var item in guarda3String)
+            {
+                Console.WriteLine(item);
+            }
+
+            Guarda3<float> guarda3Float = new Guarda3<float>
+            {
+                1.23f,
+                4.56f,
+                7.89f
+            };
+
+            Console.WriteLine("Guarda3<float> items:");
+            foreach (var item in guarda3Float)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
